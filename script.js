802978475bd42e9995f0f2bcf4e0b6bc45ada65f
thfranco <thaisfranco19@gmail.com>
2024-05-10 17:04:25 -0300

    upgrade-project

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ document.getElementById("checkbox").onclick = function() {
 
 function changeColor() {
   if (!isColorChanged) {
-    document.body.style.backgroundColor = "#404040";
+    document.body.style.backgroundColor = "#00111c";
+    document.getElementById("titulo").style.color = "#ffffff";
+    document.getElementById("subtitulo").style.color = "#ffffff";
+    document.getElementsById("card-style").style.backgroundColor = "#415a77"
+
     isColorChanged = true;
   } else {
-    document.body.style.backgroundColor = ""; // Volta para a cor original (pode ser "#ffffff" ou qualquer outra cor que você definiu)
+    document.body.style.backgroundColor = "#ffffff"; // Volta para a cor original
+    document.getElementById("titulo").style.color = "#000000";
+    document.getElementById("subtitulo").style.color = "#000000";
     isColorChanged = false;
   }
+}
+
+document.getElementById("dropdown-item").onclick = drop_options;
+
+function drop_options() {
+  document.getElementById("id_dropdown-content").classList.toggle("show");
 }
